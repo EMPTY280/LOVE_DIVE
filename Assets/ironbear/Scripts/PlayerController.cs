6bdb21768e4297fbdd644d20ad587a3a80a1e784
ironbear <20192492@g.dongseo.ac.kr>
2023-12-30 02:58:59 +0900

    플레이어 쳐맞고 튀어나갔다가 돌아오는 기능 추가

--- Assets/ironbear/Scripts/PlayerController.cs
+++ Assets/ironbear/Scripts/PlayerController.cs
@@ -18,7 +18,9 @@ public class PlayerController : MonoBehaviour
     private bool isDead = false;
     private bool isInvincible = false;
     private float invincibleStartTime;
-
+    //private float returnDuration = 3f;
+    private Vector3 startPosition;
+    
     private IInput playerInput;
     private Rigidbody rigid;
     private Collider playerCollier;
@@ -38,6 +40,8 @@ public class PlayerController : MonoBehaviour
 #if UNITY_STANDALONE || UNITY_EDITOR
         playerInput = new DesktopInput();
 #endif
+        
+        startPosition = transform.position;
         playerMoveForce = 3f;
         healthPoint = 3;
         invincibleDuration = 5f;
@@ -51,11 +55,15 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        PlayerInput();
+        if(!isInvincible)
+        {
+            PlayerInput();
+        }
 
         if (isInvincible && Time.time - invincibleStartTime > invincibleDuration)
         {
             isInvincible = false;
+            playerCollier.enabled = true;
         }
     }
 
@@ -95,11 +103,16 @@ public class PlayerController : MonoBehaviour
         if (healthPoint > 1)
         {
             healthPoint--;
+            StartCoroutine(MoveBack());
+            //StartCoroutine(Reposition());
+            //HitAnimation();
         }
         else if (healthPoint == 1 || healthPoint < 1)
         {
             isDead = true;
             healthPoint = 0;
+            StartCoroutine(MoveBack());
+            //HitAnimation();
             GameOver();
         }
     }
@@ -108,5 +121,72 @@ public class PlayerController : MonoBehaviour
     {
         //when the game is over, this method will be called
         Debug.Log("그후로 아이들은 크리스마스에 선물을 받지 못하였다..");
+    }
+
+    //trash code~
+    /*
+    IEnumerator Reposition()
+    {     
+        yield return new WaitForSeconds(1f);
+        playerCollier.enabled = false;
+
+        Vector3 curPos = transform.position;
+        Vector3 targetPos = new Vector3(0, 0, -4.5f);
+        transform.position = Vector3.MoveTowards(transform.position, startPosition, playerMoveForce * Time.deltaTime);
+    }
+
+    private void HitAnimation()
+    {
+        rigid.velocity = Vector3.zero;
+
+        Vector3 curPos = transform.position;
+        Vector3 targetPos = new Vector3(0, 0, -10);
+
+        
+        float duration = 1f;
+        float elapedTime = 0f;
+
+        while (elapedTime < duration)
+        {
+            transform.position = Vector3.Lerp(curPos, targetPos, duration);
+            elapedTime += Time.deltaTime;
+        }
+
+        transform.position = targetPos;
+        
+
+        Vector3 backVec = targetPos - curPos;
+        backVec.Normalize();
+        rigid.AddForce(backVec * playerMoveForce, ForceMode.Impulse);
+        
+    }
+    */
+    
+    IEnumerator MoveBack()
+    {
+        Vector3 initPos = transform.position;
+        Vector3 targetPos = new Vector3(0f, 0f, -10f);
+
+        while(transform.position!=targetPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * 5f);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        StartCoroutine(Reposition());
+    }
+
+    IEnumerator Reposition()
+    {
+        Vector3 returnPos = new Vector3(0f, 0f, -4.5f);
+        while (transform.position != returnPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, returnPos, Time.deltaTime * 5f);
+            yield return null;
+        }
+
+        rigid.velocity = Vector3.zero;
     }
 }
