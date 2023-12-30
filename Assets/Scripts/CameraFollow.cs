9f44c650b7ce0822a027ecd5a13e5a6809fb0049
EMPTY280 <1123pjs@gmail.com>
2023-12-31 03:46:40 +0900

    Added Screen Shake

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,11 +7,31 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform followTarget = null;
     [SerializeField] private float moveMultiplier = 0.5f;
 
+    private float shakeForce = 0f;
+    private bool isShaking = false;
+
     private void Update()
     {
         Vector3 newPos = transform.position;
-        newPos.x = followTarget.position.x * moveMultiplier;
-        newPos.y = followTarget.position.y * moveMultiplier;
+        newPos.x = followTarget.position.x * moveMultiplier + Random.Range(-shakeForce*0.5f, shakeForce*0.5f);
+        newPos.y = followTarget.position.y * moveMultiplier + Random.Range(-shakeForce * 0.5f, shakeForce * 0.5f);
         transform.position = newPos;
+    }
+
+    public void Shake(float force, float duration)
+    {
+        if (isShaking) return;
+        shakeForce = force;
+        StartCoroutine(CShake(duration));
+    }
+
+    private IEnumerator CShake(float duration)
+    {
+        float decrease = shakeForce / duration;
+        while(shakeForce > 0f)
+        {
+            shakeForce -= decrease * Time.deltaTime;
+            yield return null;
+        }
     }
 }
