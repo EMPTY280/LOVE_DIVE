ebda2b6f99c04c255745b0186b1f094fc39340f6
ironbear <20192492@g.dongseo.ac.kr>
2023-12-31 02:02:50 +0900

    시작 트윈 추가

--- Assets/ironbear/Scripts/StartUIController.cs
+++ Assets/ironbear/Scripts/StartUIController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class StartUIController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Animator uiAnimator;
+    [SerializeField]
+    private Animator santaAnimator;
+
+    private CanvasGroup uiCanvasGroup;
+
     void Start()
+    {
+        uiCanvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartBtnActive()
     {
-        
+        Sequence seq = DOTween.Sequence();
+        seq.Append(uiCanvasGroup.DOFade(0f, 1f).SetEase(Ease.Linear)).OnComplete(() =>
+        {
+            santaAnimator.SetTrigger("IsStart");
+            GameManager.Instance.ChangeScene("DeadOutTest");
+        });
+
+        seq.Play();
     }
 }
