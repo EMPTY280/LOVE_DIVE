5e909950cf2dcce7116b454364b5c4d093613d74
onhyomin <shm030208@gmail.com>
2023-12-30 19:58:31 +0900

    이상한거에요 열지맛게여
    이상한거에요 열지맛게여

--- Assets/ironbear/Scripts/PlayerController.cs
+++ Assets/ironbear/Scripts/PlayerController.cs
@@ -13,17 +13,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private int healthPoint = 3;
     [SerializeField]
-    private float invincibleDuration = 2.5f;
+    private float invincibleDuration = 3.5f;
 
     private bool isDead = false;
     private bool isInvincible = false;
     private float invincibleStartTime;
-    //private float returnDuration = 3f;
-    private Vector3 startPosition;
     
     private IInput playerInput;
     private Rigidbody rigid;
     private Collider playerCollier;
+    private Animator playerAnim;
 
 
     private void Start()
@@ -36,15 +35,18 @@ public class PlayerController : MonoBehaviour
         {
             playerCollier = GetComponent<Collider>();
         }
+        if (playerAnim == null)
+        {
+            playerAnim = GetComponent<Animator>();
+        }
 
 #if UNITY_STANDALONE || UNITY_EDITOR
         playerInput = new DesktopInput();
 #endif
         
-        startPosition = transform.position;
         playerMoveForce = 3f;
         healthPoint = 3;
-        invincibleDuration = 2.5f;
+        invincibleDuration = 3.5f;
         rigid.useGravity = false;
     }
 
@@ -64,6 +66,7 @@ public class PlayerController : MonoBehaviour
         {
             isInvincible = false;
             playerCollier.enabled = true;
+            playerAnim.SetBool("IsReturn", false);
         }
     }
 
@@ -75,13 +78,15 @@ public class PlayerController : MonoBehaviour
         Vector3 direction = new Vector3(hor, ver, 0f).normalized;
 
         PlayerMove(direction);
+
+        //left and down are negative and right and up are positive
+        //Debug.Log(hor);
+        //Debug.Log(ver);
     }
 
     private void PlayerMove(Vector3 direction)
     {
         Vector3 movement = direction * playerMoveForce * Time.deltaTime;
-        //transform.Translate(movement);
-
         rigid.AddForce(movement, ForceMode.VelocityChange);
     }
 
@@ -89,9 +94,10 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Obstacle") && isInvincible == false)
         {
-            Debug.Log("산타 아야해~");
+            //Debug.Log("산타 아야해~");
             //after the collision action will be added here
-            SantaOuch();
+            playerAnim.SetBool("IsHit", true);
+            SantaOuch();         
         }
     }
 
@@ -99,20 +105,19 @@ public class PlayerController : MonoBehaviour
     {
         isInvincible = true;
         invincibleStartTime = Time.time;
+        playerAnim.SetBool("IsHit", false);
 
         if (healthPoint > 1)
         {
             healthPoint--;
             StartCoroutine(MoveBack());
-            //StartCoroutine(Reposition());
-            //HitAnimation();
+            StartCoroutine(Reposition());
         }
         else if (healthPoint == 1 || healthPoint < 1)
         {
             isDead = true;
             healthPoint = 0;
             StartCoroutine(MoveBack());
-            //HitAnimation();
             GameOver();
         }
     }
@@ -120,70 +125,30 @@ public class PlayerController : MonoBehaviour
     private void GameOver()
     {
         //when the game is over, this method will be called
-        Debug.Log("그후로 아이들은 크리스마스에 선물을 받지 못하였다..");
+        //Debug.Log("그후로 아이들은 크리스마스에 선물을 받지 못하였다..");
     }
-
-    //trash code~
-    /*
-    IEnumerator Reposition()
-    {     
-        yield return new WaitForSeconds(1f);
-        playerCollier.enabled = false;
-
-        Vector3 curPos = transform.position;
-        Vector3 targetPos = new Vector3(0, 0, -4.5f);
-        transform.position = Vector3.MoveTowards(transform.position, startPosition, playerMoveForce * Time.deltaTime);
-    }
-
-    private void HitAnimation()
-    {
-        rigid.velocity = Vector3.zero;
-
-        Vector3 curPos = transform.position;
-        Vector3 targetPos = new Vector3(0, 0, -10);
-
-        
-        float duration = 1f;
-        float elapedTime = 0f;
-
-        while (elapedTime < duration)
-        {
-            transform.position = Vector3.Lerp(curPos, targetPos, duration);
-            elapedTime += Time.deltaTime;
-        }
-
-        transform.position = targetPos;
-        
-
-        Vector3 backVec = targetPos - curPos;
-        backVec.Normalize();
-        rigid.AddForce(backVec * playerMoveForce, ForceMode.Impulse);
-        
-    }
-    */
     
     IEnumerator MoveBack()
     {
         Vector3 initPos = transform.position;
-        Vector3 targetPos = new Vector3(0f, 0f, -10f);
+        Vector3 targetPos = new Vector3(0f, 0f, -12f);
 
         while(transform.position!=targetPos)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * 5f);
             yield return null;
         }
-
-        yield return new WaitForSeconds(1f);
-
-        StartCoroutine(Reposition());
     }
 
     IEnumerator Reposition()
     {
+        yield return new WaitForSeconds(2.5f);
+        playerAnim.SetBool("IsReturn", true);
+
         Vector3 returnPos = new Vector3(0f, 0f, -4.5f);
         while (transform.position != returnPos)
         {
-            transform.position = Vector3.MoveTowards(transform.position, returnPos, Time.deltaTime * 5f);
+            transform.position = Vector3.MoveTowards(transform.position, returnPos, Time.deltaTime * 6f);
             yield return null;
         }
 
